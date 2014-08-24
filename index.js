99e13c95da2d36c65fe75939aa08bf52b227a558
Richard Walker <digitalsadhu@gmail.com>
2014-08-24 22:05:52 +0200

    add app bootstrap

--- index.js
+++ index.js
@@ -1,5 +1,20 @@
 'use strict';
 
-module.exports = function () {
+var seneca  = require('seneca')()
+  , express = require('express')
 
-}
+var app = express()
+
+//set up the compute engine (tm) by loading with seneca
+seneca
+  .use('./compute-engine')
+  .listen()
+
+//set up an express server to provide the rest endpoint
+app.get('/impact/:geojson', function (req, res) {
+  seneca.act({ action: 'compute-weighting', geojson: req.params.geojson }, function (err, result) {
+    res.send({value: result})
+  })
+})
+
+app.listen(3002)
